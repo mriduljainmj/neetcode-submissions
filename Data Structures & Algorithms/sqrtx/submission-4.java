d681d95d6aa614d29f0a09802b9ba8e4c9144eaf
Mridul <itsme.mriduljain@gmail.com>
2026-06-08 20:55:23 +0530

    Add: sqrtx - submission-4

---
+++ Data Structures & Algorithms/sqrtx/submission-4.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int mySqrt(int x) {
+        if(x<=1){
+            return x;
+        }
+        int l =0;
+        int r = x/2;
+        int res = 0;
+        while(l<=r){
+            int m = l + (r-l)/2; 
+            if((long)m*m > x){
+                r = m-1;
+            }
+            else if((long)m*m < x){
+                l = m+1;
+                res = m;
+            }
+            else{
+                return m;
+            }
+        }
+
+        return res;
+    }
+}
