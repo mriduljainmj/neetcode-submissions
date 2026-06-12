3a2bcf48c9c2bd86c522d69eb69ac353e6ce7130
Mridul <itsme.mriduljain@gmail.com>
2026-06-12 14:52:45 +0530

    Add: design-circular-queue - submission-2

---
+++ Data Structures & Algorithms/design-circular-queue/submission-2.java
@@ -0,0 +1,79 @@
+class MyCircularQueue {
+    private int space;
+    private ListNode left;
+    private ListNode right;
+    public MyCircularQueue(int k) {
+        space = k;
+        left = new ListNode(0, null,null);
+        right = new ListNode(0,null,left);
+        left.next = right;
+    }
+    
+    public boolean enQueue(int value) {
+        if(isFull()){
+            return false;
+        }
+        ListNode newNode = new ListNode(value, right, right.prev);
+        newNode.next.prev = newNode;
+        newNode.prev.next = newNode;
+        space--;
+        return true;
+    }
+    
+    public boolean deQueue() {
+        if(isEmpty()){
+            return false;
+        }
+        left.next = left.next.next;
+        left.next.prev = left;
+        space++;
+        return true;
+
+    }
+    
+    public int Front() {
+        if(isEmpty()){
+            return -1;
+        }
+        return left.next.val;
+    }
+    
+    public int Rear() {
+        if(isEmpty()){
+            return -1;
+        }
+        return right.prev.val;
+    }
+    
+    public boolean isEmpty() {
+       return left.next == right;
+    }
+    
+    public boolean isFull() {
+        if(space==0){
+            return true;
+        }
+        return false;
+    }
+}
+
+public class ListNode{
+    int val;
+    ListNode next,prev;
+    ListNode(int val, ListNode next, ListNode prev){
+        this.val = val;
+        this.next = next;
+        this.prev = prev;
+    }
+} 
+
+/**
+ * Your MyCircularQueue object will be instantiated and called as such:
+ * MyCircularQueue obj = new MyCircularQueue(k);
+ * boolean param_1 = obj.enQueue(value);
+ * boolean param_2 = obj.deQueue();
+ * int param_3 = obj.Front();
+ * int param_4 = obj.Rear();
+ * boolean param_5 = obj.isEmpty();
+ * boolean param_6 = obj.isFull();
+ */
