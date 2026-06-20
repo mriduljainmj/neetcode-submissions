9ea2a7c59ad2d8fbc47ab2db659ab0f3cbad8d6a
Mridul <itsme.mriduljain@gmail.com>
2026-06-20 20:32:44 +0530

    Add: is-anagram - submission-8

---
+++ Data Structures & Algorithms/is-anagram/submission-8.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        HashMap<Character,Integer> sMap = new HashMap<>();
+        HashMap<Character,Integer> tMap = new HashMap<>();
+
+        for(Character c : s.toCharArray()){
+            sMap.put(c, sMap.getOrDefault(c,0)+1);
+        }
+        
+        for(Character c : t.toCharArray()){
+            tMap.put(c, tMap.getOrDefault(c,0)+1);
+        }
+
+        if(sMap.equals(tMap)){
+            return true;
+        }
+
+        return false;
+
+
+    }
+}
