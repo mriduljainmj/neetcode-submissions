a838df3d4b62e155552fcdee65f820488c1603f4
Mridul <itsme.mriduljain@gmail.com>
2026-07-02 23:44:53 +0530

    Add: majority-element - submission-5

---
+++ Data Structures & Algorithms/majority-element/submission-5.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int res = 0;
+        int maxCount = 0;
+        for(int n : nums){
+            map.put(n, map.getOrDefault(n, 0)+1);
+            if(map.get(n) > maxCount){
+                res = n;
+                maxCount = map.get(n);
+            }
+        }
+        return res;
+    }
+}
