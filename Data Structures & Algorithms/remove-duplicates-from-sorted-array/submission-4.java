e8b6f6c89197ff9e064646e41f3abcc849908765
Mridul <itsme.mriduljain@gmail.com>
2026-07-22 21:44:12 +0530

    Add: remove-duplicates-from-sorted-array - submission-4

---
+++ Data Structures & Algorithms/remove-duplicates-from-sorted-array/submission-4.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int L=1;
+        for(int R=1;R<nums.length;R++){
+            if(nums[R]!=nums[R-1]){
+                nums[L] = nums[R];
+                L++;
+            }
+        }
+
+        return L;
+    }
+}
