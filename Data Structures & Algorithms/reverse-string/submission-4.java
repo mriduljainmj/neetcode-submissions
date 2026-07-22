e2ae1b8807f9b525089a107feadef3910e38c2c6
Mridul <itsme.mriduljain@gmail.com>
2026-07-22 19:39:11 +0530

    Add: reverse-string - submission-4

---
+++ Data Structures & Algorithms/reverse-string/submission-4.java
@@ -0,0 +1,13 @@
+class Solution {
+    public void reverseString(char[] s) {
+        int i=0;
+        int j=s.length-1;
+        while(j>=i){
+            char temp = s[i];
+            s[i] = s[j];
+            s[j] = temp;
+            i++;
+            j--;
+        }
+    }
+}
